81f488630826d3542b7e07e34d7b2fe394cd6630
Safiyah Azeez <>
2022-11-16 20:46:34 +0000

    added sazeez2 fuction

---
+++ .ccls-cache/@home@runner@cs-211/folder@211-f22-collab-github@collab.c
@@ -0,0 +1,31 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void sazeez2() {
+  printf("Safiyah\n");
+  printf("I learned that Git is a version control software which is essentially a system that recored changes made to a software and the different versions of code that exists and it can work on a local machine.  Git Hub is a cloud based hosting service that allows the management of Git repositories and it used very widely in industry practice. \n");
+}
+
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    sazeez2();
+    
+    return 0;
+}
